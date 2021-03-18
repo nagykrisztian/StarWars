ed8c34ca1d2c6fc3a93155d69207ef777f1b165b
kelemenandraas <kelemenandraas0219@gmail.com>
2021-03-18 22:23:50 +0100

    ✨Meteor

--- StarWars.py
+++ StarWars.py
@@ -1,4 +1,6 @@
 import turtle
+import time
+from random import *
 
 
 def balra():
@@ -17,10 +19,15 @@ def le():
     ship.sety(ship.ycor()-10)
 
 
+def MeteorYcorSorsol():
+    meteor.sety(randint(-250, 250))
+
+
 space = turtle.Screen()
 space.setup(width=800,height=600)
 space.bgpic("img/space.png")
 space.addshape("img/sprite.gif")
+space.addshape("img/meteor2.gif")
 
 space.listen()
 space.tracer(0)
@@ -29,6 +36,12 @@ space.onkeypress(balra, "Left")
 space.onkeypress(jobbra, "Right")
 space.onkeypress(fel, "Up")
 space.onkeypress(le, "Down")
+
+meteor= turtle.Turtle()
+meteor.shape("img/meteor2.gif")
+meteor.penup()
+meteor.setx(380)
+MeteorYcorSorsol()
 
 ship = turtle.Turtle()
 ship.shape("img/sprite.gif")
@@ -44,5 +57,8 @@ while True:
         ship.sety(-290)
     if ship.ycor() < -300:
         ship.sety(290)
-
+    meteor.setx(meteor.xcor()-0.3)
+    if meteor.xcor() < -400:
+        MeteorYcorSorsol()
+        meteor.setx(380)
     space.update()
